03deddc12ff2c26a5580e62c555d956a393e2fef
pdesmond91 <40234626+pdesmond91@users.noreply.github.com>
2024-06-19 15:50:39 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,4 +42,4 @@ if ingredients_list:
     # st.write(my_insert_stmt)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
