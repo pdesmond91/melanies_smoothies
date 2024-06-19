400378c696dd1478886b4c4c47dd0e6b85ab69af
pdesmond91 <40234626+pdesmond91@users.noreply.github.com>
2024-06-19 16:25:48 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,21 +22,22 @@ ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
     ,my_dataframe
     ,max_selections = 5)
-if ingredients_list:
-    ingredients_string = ''
 
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
+#if ingredients_list:
+#    ingredients_string = ''
+
+#    for fruit_chosen in ingredients_list:
+#        ingredients_string += fruit_chosen + ' '
 
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-                values ('""" + ingredients_string + """','""" + name_on_order + """')"""
+#    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+#                values ('""" + ingredients_string + """','""" + name_on_order + """')"""
     
-    time_to_insert = st.button('Submit Order')
+#    time_to_insert = st.button('Submit Order')
 
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
+#    if time_to_insert:
+#        session.sql(my_insert_stmt).collect()
+#        st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
 if ingredients_list:
     ingredients_string = ''
@@ -50,3 +51,12 @@ if ingredients_list:
         st.subheader(fruit_chosen + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+                values ('""" + ingredients_string + """','""" + name_on_order + """')"""
+
+    time_to_insert = st.button('Submit Order')
+
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
